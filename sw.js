ba39481d3cc83fd580c6657e9dad21b8b1ea2adb
srikunj69-cloud <srikunj69@gmail.com>
2026-05-02 02:06:56 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('pos-store').then((cache) => cache.addAll(['index.html'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
-});
